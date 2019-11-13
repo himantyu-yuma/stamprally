b8375e722cc2ec12d19eb19e4a0b81e27cf461f7
aroe <lipton.happy.milktea@gmail.com>
2019-11-13 22:27:10 +0900

    add ajax

---
+++ main.js
@@ -0,0 +1,25 @@
+let getClub = function(){
+    clubDic = {};
+    $.ajax({
+        url: 'clubList.json',
+        type: 'POST',
+        dataType: 'json'
+    })
+    .done(function(data){
+        clubDic = data;
+        console.log(clubDic['0001'])
+    })
+    .fail(function(data){
+        console.log(data);
+    });
+}
+
+let getId = function (){
+    let id;
+    if(location.search == ''){
+        return;
+    }else{
+        id = location.search.replace('?id=','');
+    }
+    $(`#${id}`).attr('src', 'img/stamp.png')
+}
