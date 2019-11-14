2480a6c7ea1640088144c61a1243dd1896c43394
aroe <lipton.happy.milktea@gmail.com>
2019-11-15 00:26:00 +0900

    fixed bug

--- main.js
+++ main.js
@@ -1,55 +1,53 @@
-let getClub = function(){
+let getClub = function () {
     clubDic = {};
     $.ajax({
         url: 'clubList.json',
         type: 'GET',
         dataType: 'json'
     })
-    .done(function(data){
-        // 部活のidと名前のjsonを連想配列に
-        clubDic = data;
-        // 部活のid
-        keys = Object.keys(clubDic);
-    })
-    .fail(function(data){
-        alert('読み込みに失敗しました')
-        console.log(data);
-    });
+        .done(function (data) {
+            // 部活のidと名前のjsonを連想配列に
+            clubDic = data;
+            // 部活のid
+            keys = Object.keys(clubDic);
+        })
+        .fail(function (data) {
+            alert('読み込みに失敗しました')
+            console.log(data);
+        });
 }
 
-let makeDOM = function(){
-    let clubNum = keys.length;
+let makeDOM = function () {
     let rowNum = 3;
     let currentRowNum = 0;
 
     $('#sheet').append(
         row = $('<tr>')
     );
-    for (let i = 0; i < clubNum; i++) {
-        if(currentRowNum > rowNum){
+    for (let i = 0; i < keys.length; i++) {
+        if (currentRowNum > rowNum) {
             currentRowNum = 0;
             $('#sheet').append(
                 row = $('<tr>')
             );
-        }else{
-            currentRowNum++;
-            row.append($('<td>')
-            .append($('<img>',{
+        }
+        currentRowNum++;
+        row.append($('<td>')
+            .append($('<img>', {
                 'src': 'img/none.png',
                 'id': keys[i]
             })
             ));
-        }
-        currentRowNum ++;
+
     }
 }
 
-let getId = function (){
+let getId = function () {
     let id;
-    if(location.search == ''){
+    if (location.search == '') {
         return;
-    }else{
-        id = location.search.replace('?id=','');
+    } else {
+        id = location.search.replace('?id=', '');
     }
     $(`#${id}`).attr('src', 'img/stamp.png')
 }
